4f1abd9c59f3edfbac26a345202dd9b81cdb1fec
Jon Bevan <jon@jonbevan.me.uk>
2012-09-06 00:06:24 +0100

    Better job descriptions

--- database.sql
+++ database.sql
@@ -6,5 +6,5 @@ GRANT ALL ON test.* TO 'user'@'%';
 
 CREATE TABLE employees (id INT(11) AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL, job VARCHAR(100) NOT NULL);
 
-INSERT INTO employees (name,job) VALUES ('Jon','Web Developer'),('Mark','Managing Director'),('Tom','Marketing'),
-('Sam', 'Web Designer'),('Bhups', 'Web Designer'),('Hannah','Finance'),('Jacqui','Client Manager'),('Abby', 'Copy Writer');
+INSERT INTO employees (name,job) VALUES ('Jon','Web Developer'),('Mark','Managing Director'),('Tom','Marketing Expert'),
+('Sam', 'Web Designer'),('Bhups', 'Web Designer'),('Hannah','Finance Department'),('Jacqui','Client Manager'),('Abby', 'Copy Writer');
