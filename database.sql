a7e012d43d11bab7afafa0d20bb57fe9e2c3cceb
Jon Bevan <jon@jonbevan.me.uk>
2012-09-06 00:00:03 +0100

    Added database structure and contents

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE test;
+USE test;
+
+CREATE USER 'user'@'%' IDENTIFIED BY 'password';
+GRANT ALL ON test.* TO 'user'@'%';
+
+CREATE TABLE employees (id INT(11) AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL, job VARCHAR(100) NOT NULL);
+
+INSERT INTO employees (name,job) VALUES ('Jon','Web Developer'),('Mark','Managing Director'),('Tom','Marketing'),
+('Sam', 'Web Designer'),('Bhups', 'Web Designer'),('Hannah','Finance'),('Jacqui','Client Manager'),('Abby', 'Copy Writer');
